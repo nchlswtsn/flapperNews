b956a0ef916ba64c2660d56f12d35fbaab82fce5
Charles Watson <nchlswtsn@gmail.com>
2015-10-11 16:05:24 -0700

    added backend express structure and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-var app = angular.module('codingHouseNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+  app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
-app.controller('MainCtrl', ['$scope', function($scope){
-  $scope.posts = [];
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      })
+
+
+    $urlRouterProvider.otherwise('/home');
+  }]);
+
+
+app.factory('posts', [function(){
+  var objectPosts = {
+    posts: []
+  };
+  return objectPosts;
+}]);
+
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope, posts){
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === ''){
       return;
@@ -9,12 +37,39 @@ app.controller('MainCtrl', ['$scope', function($scope){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
   };
+
   $scope.incrementUpvotes = function(post){
     post.upvotes += 1;
+  };
+}]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+  $scope.incrementUpvotes = function(comment){
+    comment.upvotes += 1;
   };
 }]);
